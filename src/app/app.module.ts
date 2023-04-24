ac921031d92c17c54251738518b0164d6b32ad62
Philip Carlsson <philipcarlsson@hotmail.com>
2023-04-24 23:48:17 +0200

    creating angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RecipiesComponent } from './recipies/recipies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipiesComponent
   ],
   imports: [
     BrowserModule
