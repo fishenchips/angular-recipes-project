f5bc7f4429c0f49c74f22eec2b2b1c89a9ddbf84
Philip Carlsson <philipcarlsson@hotmail.com>
2023-04-28 10:40:25 +0200

    passing down recipe item info w/ property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipes-project';
-  section: string;
+  section: string = 'recipes';
 
   onClickHeader(section: string) {
     this.section = section;
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,6 +8,11 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent {
   recipes: Array<Recipe> = [
+    new Recipe(
+      'Pizza',
+      'Its yummy',
+      'https://emvwr2994ad.exactdn.com/wp-content/uploads/2021/10/bast-finpizza-stockholm.jpg?strip=all&lossy=1&quality=77&resize=795%2C480&ssl=1'
+    ),
     new Recipe(
       'Pizza',
       'Its yummy',
