41495d2aae2a16333eb0d5b2f1a87bbad12fe015
Philip Carlsson <philipcarlsson@hotmail.com>
2023-05-08 20:30:06 +0200

    using shopping list service functions and array in component

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Array<Ingredient>>();
 
-  constructor() { }
+  private ingredients: Array<Ingredient> = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+    new Ingredient('Bananas', 2),
+  ];
+
+  constructor() {}
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addNewIngredient(newIngredient: Ingredient) {
+    this.ingredients.push(newIngredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
 }
