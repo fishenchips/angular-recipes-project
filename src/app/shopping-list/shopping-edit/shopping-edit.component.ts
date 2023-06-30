1a7fc6108296b9cc57dbebe4c4d4cec26872d11d
fishenchips <philipcarlsson@hotmail.com>
2023-06-30 15:00:36 +0200

    updating ingredient overwrites previous data

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -33,7 +33,14 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   onAddItem(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
-    this.shoppingListService.addNewIngredient(newIngredient);
+    if (this.editMode) {
+      this.shoppingListService.updateIngredient(
+        this.editedItemIndex,
+        newIngredient
+      );
+    } else {
+      this.shoppingListService.addNewIngredient(newIngredient);
+    }
   }
 
   ngOnDestroy(): void {
