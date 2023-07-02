6743aa083dc0b697b04a258e5d4b7cee195769db
fishenchips <philipcarlsson@hotmail.com>
2023-07-02 14:30:43 +0200

    functionallity to delete and reload page

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { RecipeService } from '../recipe.service';
 
 @Component({
@@ -15,7 +15,8 @@ export class RecipeEditComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private recipeService: RecipeService
+    private recipeService: RecipeService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -44,6 +45,7 @@ export class RecipeEditComponent implements OnInit {
     } else {
       this.recipeService.addRecipe(this.recipeForm.value);
     }
+    this.onCancel();
   }
 
   private initForm() {
@@ -77,6 +79,12 @@ export class RecipeEditComponent implements OnInit {
       imagePath: new FormControl(recipeImgPath, Validators.required),
       description: new FormControl(recipeDescription, Validators.required),
       ingredients: recipeIngredients,
+    });
+  }
+
+  onCancel() {
+    this.router.navigate(['../'], {
+      relativeTo: this.route,
     });
   }
 }
