8cbfbec44ab9ec7914f93a58853199b9d8a9c45a
fishenchips <philipcarlsson@hotmail.com>
2023-07-14 09:45:41 +0200

    fixing resolver bug where edited recipes are not displayed

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -7,15 +7,24 @@ import {
 import { Recipe } from './recipe.model';
 import { DataStorageService } from '../shared/data-storage.service';
 import { Observable } from 'rxjs';
+import { RecipeService } from './recipe.service';
 
 @Injectable({ providedIn: 'root' })
 export class RecipesResolverService implements Resolve<Recipe[]> {
-  constructor(private dataStorage: DataStorageService) {}
+  constructor(
+    private dataStorage: DataStorageService,
+    private recipeService: RecipeService
+  ) {}
 
   resolve(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Recipe[] | Observable<Recipe[]> | Promise<Recipe[]> {
-    return this.dataStorage.getRecipes();
+    const recipes = this.recipeService.getRecipes();
+    if (recipes.length === 0) {
+      return this.dataStorage.getRecipes();
+    } else {
+      return recipes;
+    }
   }
 }
