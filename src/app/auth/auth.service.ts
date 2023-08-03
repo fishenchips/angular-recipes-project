1040a657a1fc2eb196d2e27c07e4ad838f2179ce
fishenchips <philipcarlsson@hotmail.com>
2023-08-03 12:48:21 +0200

    handling error message in auth service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 interface AuthResponseData {
   kind: string;
@@ -15,9 +17,26 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   signup(email: string, password: string) {
-    return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBJQNtS941oddgj9bDllfH92COe-DrVyi4',
-      { email, password, returnSecureToken: true }
-    );
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBJQNtS941oddgj9bDllfH92COe-DrVyi4',
+        { email, password, returnSecureToken: true }
+      )
+      .pipe(
+        catchError((errorRes) => {
+          let errorMsg = 'An unkown error occured.';
+
+          if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMsg);
+          }
+
+          switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+              errorMsg = 'This email already exists.';
+          }
+
+          return throwError(errorMsg);
+        })
+      );
   }
 }
